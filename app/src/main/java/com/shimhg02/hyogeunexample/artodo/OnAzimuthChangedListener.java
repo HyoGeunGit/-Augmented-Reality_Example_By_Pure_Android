8eb0362568375ac184b0aeceb36e9c91dd9dfcaf
HyoGeunGit <shimhg02@naver.com>
2019-05-20 09:15:20 +0900

    Google+ Core

---
+++ app/src/main/java/com/shimhg02/hyogeunexample/artodo/OnAzimuthChangedListener.java
@@ -0,0 +1,8 @@
+package com.shimhg02.hyogeunexample.artodo;
+
+/**
+ * Created by Shimhg02.
+ */
+public interface OnAzimuthChangedListener {
+    void onAzimuthChanged(float azimuthFrom, float azimuthTo);
+}
---
+++ app/src/main/java/com/shimhg02/hyogeunexample/artodo/OnLocationChangedListener.java
@@ -0,0 +1,9 @@
+package com.shimhg02.hyogeunexample.artodo;
+import android.location.Location;
+
+/**
+ * Created by Shimhg02.
+ */
+public interface OnLocationChangedListener {
+    void onLocationChanged(Location currentLocation);
+}
