656bbd04c30dbc0b94c6e8dacb6dceb15ab6d097
HyoGeunGit <shimhg02@naver.com>
2019-05-20 09:48:05 +0900

    Refactor To Kotlin

--- app/src/main/java/com/shimhg02/hyogeunexample/artodo/MyCurrentAzimuth.java
+++ app/src/main/java/com/shimhg02/hyogeunexample/artodo/MyCurrentAzimuth.java
@@ -1,56 +1,43 @@
-package com.shimhg02.hyogeunexample.artodo;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-
+package com.shimhg02.hyogeunexample.artodo
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 /**
  * Created by Shimhg02.
  */
-public class MyCurrentAzimuth implements SensorEventListener {
-
-    private SensorManager sensorManager;
-    private Sensor sensor;
-    private int azimuthFrom = 0;
-    private int azimuthTo = 0;
-    private OnAzimuthChangedListener mAzimuthListener;
-    Context mContext;
-
-    public MyCurrentAzimuth(OnAzimuthChangedListener azimuthListener, Context context) {
-        mAzimuthListener = azimuthListener;
-        mContext = context;
+class MyCurrentAzimuth(azimuthListener:OnAzimuthChangedListener, context:Context):SensorEventListener {
+    private var sensorManager: SensorManager? = null
+    private var sensor: Sensor? = null
+    private var azimuthFrom = 0
+    private var azimuthTo = 0
+    private var mAzimuthListener:OnAzimuthChangedListener
+    internal var mContext:Context
+    init{
+        mAzimuthListener = azimuthListener
+        mContext = context
     }
-
-    public void start(){
-        sensorManager = (SensorManager) mContext.getSystemService(mContext.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        sensorManager.registerListener(this, sensor,
-                SensorManager.SENSOR_DELAY_UI);
+    fun start() {
+        sensorManager = mContext.getSystemService(mContext.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+        sensorManager!!.registerListener(this, sensor,
+                SensorManager.SENSOR_DELAY_UI)
     }
-
-    public void stop(){
-        sensorManager.unregisterListener(this);
+    fun stop() {
+        sensorManager.unregisterListener(this)
     }
-
-    public void setOnShakeListener(OnAzimuthChangedListener listener) {
-        mAzimuthListener = listener;
+    fun setOnShakeListener(listener:OnAzimuthChangedListener) {
+        mAzimuthListener = listener
     }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        azimuthFrom = azimuthTo;
-
-        float[] orientation = new float[3];
-        float[] rMat = new float[9];
-        SensorManager.getRotationMatrixFromVector(rMat, event.values);
-        azimuthTo = (int) ( Math.toDegrees( SensorManager.getOrientation( rMat, orientation )[0] ) + 360 ) % 360;
-
-        mAzimuthListener.onAzimuthChanged(azimuthFrom, azimuthTo);
+    override fun onSensorChanged(event:SensorEvent) {
+        azimuthFrom = azimuthTo
+        val orientation = FloatArray(3)
+        val rMat = FloatArray(9)
+        SensorManager.getRotationMatrixFromVector(rMat, event.values)
+        azimuthTo = (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0].toDouble()) + 360).toInt() % 360
+        mAzimuthListener.onAzimuthChanged(azimuthFrom, azimuthTo)
     }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+    override fun onAccuracyChanged(sensor:Sensor, accuracy:Int) {
     }
 }
